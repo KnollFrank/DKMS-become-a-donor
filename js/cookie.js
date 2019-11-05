0e346045d43b3fa2f939e5e8ab3000eb0c9bfff5
Frank Knoll <Knoll_Frank@web.de>
2019-11-05 08:33:21 +0100

    starting "cookies akzeptieren"

---
+++ js/cookie.js
@@ -0,0 +1,9 @@
+/* temporarily setting hidecookiedingsbums=0 */
+/* document.cookie = 'hidecookiedingsbums=0;path=/' */
+console.log('My cookies: ', document.cookie);
+if (document.cookie.indexOf('hidecookiedingsbums=1') != -1) {
+  $('#cookiedingsbums').hide();
+} else {
+  $('#cookiedingsbums').prependTo('body');
+  $('#cookiedingsbumsCloser').show();
+}

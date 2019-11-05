6bc33629888ac899ba1627a343b85fb9e3689dec
Frank Knoll <Knoll_Frank@web.de>
2019-11-05 09:12:52 +0100

    refactoring

--- js/cookie.js
+++ js/cookie.js
@@ -2,8 +2,13 @@
 /* document.cookie = 'hidecookiedingsbums=0;path=/' */
 console.log('My cookies: ', document.cookie);
 if (document.cookie.indexOf('hidecookiedingsbums=1') != -1) {
-  $('#cookiedingsbums').hide();
+	$('#cookiedingsbums').hide();
 } else {
-  $('#cookiedingsbums').prependTo('body');
-  $('#cookiedingsbumsCloser').show();
+	$('#cookiedingsbums').prependTo('body');
+	$('#cookiedingsbumsCloser').show();
+}
+
+function hidecookiedingsbums() {
+	document.cookie = 'hidecookiedingsbums=1;path=/';
+	$('#cookiedingsbums').slideUp()
 }

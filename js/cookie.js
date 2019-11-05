18d5b598f9e67f84c44f93b2a9701478b3f02b42
Frank Knoll <Knoll_Frank@web.de>
2019-11-05 09:19:53 +0100

    refactoring

--- js/cookie.js
+++ js/cookie.js
@@ -1,7 +1,7 @@
-/* temporarily setting hidecookiedingsbums=0 */
-/* document.cookie = 'hidecookiedingsbums=0;path=/' */
 console.log('My cookies: ', document.cookie);
-if (document.cookie.indexOf('hidecookiedingsbums=1') != -1) {
+var cookieAssignment = 'hidecookiedingsbums=1';
+
+if (document.cookie.indexOf(cookieAssignment) != -1) {
 	$('#cookiedingsbums').hide();
 } else {
 	$('#cookiedingsbums').prependTo('body');
@@ -9,6 +9,6 @@ if (document.cookie.indexOf('hidecookiedingsbums=1') != -1) {
 }
 
 function hidecookiedingsbums() {
-	document.cookie = 'hidecookiedingsbums=1;path=/';
+	document.cookie = cookieAssignment + ';path=/';
 	$('#cookiedingsbums').slideUp()
 }
